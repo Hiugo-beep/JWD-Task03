a0d45d8799cd1a46ef30232a8f271d634a83cda3
Hiugo-beep <55299837+Hiugo-beep@users.noreply.github.com>
2021-01-27 10:56:19 +0300

    Update ParserReaderImpl.java

--- jwd-task-03-xml-parser/src/main/java/by/jwd/parsexml/reader/impl/ParserReaderImpl.java
+++ jwd-task-03-xml-parser/src/main/java/by/jwd/parsexml/reader/impl/ParserReaderImpl.java
@@ -1,36 +1,59 @@
 package by.jwd.parsexml.reader.impl;
 
-import java.util.List;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.regex.Pattern;
 
 import by.jwd.parsexml.reader.ParserReader;
 
 public class ParserReaderImpl implements ParserReader{
-	private static int numberBytesToRead = 50;
-	private int startByteToRead;
+	private static int numberBytesToRead = 500;
+	private int offsetByteToRead;
 	private int bytesRead;
-	private String buffer;
+	private byte[] buffer;
 	private String fileRead;
+	private int bytesAvailable;
 	
 	public ParserReaderImpl(){
-		this.startByteToRead = 0;
+		this.offsetByteToRead = 0;
+		this.bytesAvailable = 0;
 		this.bytesRead = numberBytesToRead;
-		this.buffer = "";
+		this.buffer = new byte [numberBytesToRead];
 		this.fileRead = "";
 	}
 	
-	private int readBytes(int startByteToRead, int numberBytesRead) {
-		//здесь произойдет считка байт в потоке
-		return 0;
-	}
 	private void filterSpaces() {
-		//чистка в буфере более одного пробела
+		String wordSeparator = "\s\s+";//2 и более символов - разделителей
+		String[] result;
+		Pattern pattern = Pattern.compile(wordSeparator);
+		result = pattern.split(fileRead);
+		this.mergeTokens(result);
 	}
-	@Override
-	public String readFile(String fileName){
-		while(bytesRead == numberBytesToRead) { 
-			bytesRead = this.readBytes(startByteToRead, bytesRead); 
-			fileRead += buffer;
+	private void mergeTokens(String[] tokens) {
+		this.fileRead = "";
+		for(String str : tokens) {
+				fileRead += str;
 		}
+	}
+		
+	@Override
+	public String readFile( String fileName ) throws IOException {
+		
+		FileInputStream inFile = new FileInputStream( fileName );
+		
+		while(bytesRead == numberBytesToRead) { 
+
+			bytesAvailable = inFile.available();
+			if( bytesAvailable > numberBytesToRead ) 
+				bytesAvailable = numberBytesToRead;
+
+			bytesRead = inFile.read( buffer, offsetByteToRead, bytesAvailable );
+			
+			offsetByteToRead += bytesRead;
+			fileRead += new String( buffer, "UTF-8" );
+		}
+		inFile.close();
+		this.filterSpaces();
 		return fileRead;
 	}
 
