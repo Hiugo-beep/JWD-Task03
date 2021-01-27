f2dc2ad9abcacc55495f6cec4808a23766720063
Hiugo-beep <55299837+Hiugo-beep@users.noreply.github.com>
2021-01-27 10:42:19 +0300

    Update TreeTraversal.java

--- jwd-task-03-xml-parser/src/main/java/by/jwd/parsexml/behavior/TreeTraversal.java
+++ jwd-task-03-xml-parser/src/main/java/by/jwd/parsexml/behavior/TreeTraversal.java
@@ -1,7 +1,30 @@
 package by.jwd.parsexml.behavior;
 
+import by.jwd.parsexml.behavior.TreeTraversal;
 import by.jwd.parsexml.entity.Element;
 
-public interface TreeTraversal {
-	public String getTreeContent(Element ob);
+public interface TreeTraversal  implements TreeTraversal {
+	private String treeContent = "";
+	private int counter = 1;
+	
+	@Override
+	public String getTreeContent(Element ob) {
+		setTreeContent(ob);
+		return treeContent;
+	}
+	
+	private void setTreeContent(Element ob) 
+	{
+		if (ob!= null) {		  
+			if(ob.getContent() == "") {
+				for(Element child: ob.getChildElements())
+				{
+					setTreeContent(child);
+				}
+				counter++;
+				treeContent += "\n";
+			}else 
+				treeContent += counter+") " + ob.getContent()+"\n";
+		}
+	}
 }
