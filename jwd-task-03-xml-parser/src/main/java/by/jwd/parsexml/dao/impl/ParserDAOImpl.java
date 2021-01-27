cc46ba73c8f8d13d66399955ecb3e54919968a02
Hiugo-beep <55299837+Hiugo-beep@users.noreply.github.com>
2021-01-27 10:44:05 +0300

    Update ParserDAOImpl.java

--- jwd-task-03-xml-parser/src/main/java/by/jwd/parsexml/dao/impl/ParserDAOImpl.java
+++ jwd-task-03-xml-parser/src/main/java/by/jwd/parsexml/dao/impl/ParserDAOImpl.java
@@ -1,6 +1,6 @@
 package by.jwd.parsexml.dao.impl;
 
-
+import java.io.IOException;
 import by.jwd.parsexml.dao.ParserDAO;
 import by.jwd.parsexml.entity.Element;
 import by.jwd.parsexml.parser.Parser;
@@ -12,9 +12,15 @@ public class ParserDAOImpl implements ParserDAO{
 	@Override
 	public Element parse(String fileName) {
 		ParserReader reader = new ParserReaderImpl();
-		String fileRead = reader.readFile(fileName);
+		String fileRead = "";
+		try { 
+			fileRead = reader.readFile(fileName);
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
 		
-		Parser tree = new Parser(); //Stack используется в этмоу классе
+		Parser tree = new Parser();
 		Element rootElement = tree.createElement(fileRead);
 		return rootElement;
 	}
